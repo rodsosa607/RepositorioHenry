a7ad61eff3959a567fbe10069abbb5b328520b88
Rodrigo Marcelo Sosa <rodsosa607@hotmail.com>
2023-02-16 22:39:56 -0300

    second commit

---
+++ Variables.js
@@ -0,0 +1,18 @@
+//string
+var bootcamp = 'Henry';
+
+//números
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 14.66;
+
+//Booleanos
+var estoyAprendiendo = true;
+var estoyAburrido = false;
+
+//Undefined
+var cajon;
+console.log(cajon);
+
+//null
+var sinNada = null;
---
+++ operadores.js
@@ -0,0 +1,10 @@
+//+ - * / %
+
+console.log(3+5);
+console.log(95 % 4);
+console.log(3+5*2-8);
+
+var cadena1 = 'mi nombre es ';
+var cadena2 = 'Rodrigo Marcelo Sosa';
+
+console.log(cadena1 + cadena2);
